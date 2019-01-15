8b333a26d3fb00db05c68ec9ba78c2a983f35419
RachelKolk <rach.m.obrien@gmail.com>
2019-01-14 21:33:44 -0500

    pretty happy with searchbar styling

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,17 +1,31 @@
 import React from "react";
-// import "./SearchBar.css";
+import "./SearchBar.css";
 
 function SearchBar() {
     return (
-        <div>
-            <img alt="Instagram logo"/>
-            <form>
-                <input
+        <div className="SearchBar">
+
+            <div className="logo">
+            <i class="fab fa-instagram"></i>
+            <img className="logoScript" src="https://upload.wikimedia.org/wikipedia/commons/2/2a/Instagram_logo.svg" alt="Instagram logo"/>
+            </div>
+
+            <form className="searchForm">
+                  
+                <input 
                     type="text"
                     name="search"
-                    placeholder="Search" >
+                    placeholder= "  Search" >
+                
                 </input>
+                <i className="fas fa-search"></i>
             </form>
+            
+            <div className="profileIcons">
+            <i className="far fa-compass"></i>
+            <i className="far fa-heart"></i>
+            <i className="far fa-user"></i>
+            </div>
             
         </div>
     );
