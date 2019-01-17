7cad1ec88f669bcb297a6815ff041ea56ec5b3f9
RachelKolk <rach.m.obrien@gmail.com>
2019-01-17 17:06:04 -0500

    searchbar and header done

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,25 +1,63 @@
 import React from "react";
+import styled from 'styled-components';
 import "./SearchBar.css";
+
+const Header = styled.div`
+    width: auto;
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    padding:1%;
+    border-bottom: 1px solid lightgray;
+    margin: 1%
+`;
+
+const LogoHeader = styled.div`
+    display: flex;
+    flex-wrap: nowrap;
+`;
+
+const LogoImage = styled.img`
+    line-height: 5rem;
+`;
+
+
+const SearchForm = styled.form`
+      
+    color: lightgray;
+    font-size: 16px;
+`;
+
+const SearchInput = styled.input`
+    width: 250px;
+    height: 32px;
+
+    background: #fcfcfc;
+    border: 1px solid #aaa;
+    border-radius: 5px;
+    text-indent: 32px;
+`;
 
 function SearchBar(props) {
     return (
-        <div className="SearchBar">
+        <Header>
 
-            <div className="logo">
+            <LogoHeader>
+           
             <i className="fab fa-instagram"></i>
-            <img className="logoScript" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/320px-Instagram_logo.svg.png" alt="Instagram logo"/>
-            </div>
+            <LogoImage src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/320px-Instagram_logo.svg.png" alt="Instagram logo"/>
+            </LogoHeader>
 
-            <form onSubmit={props.search}  className="searchForm">
-                  
-                <input 
+            <SearchForm onSubmit={props.search}>
+            {/* <i className="fas fa-search"></i> */}
+                <SearchInput
                     type="text"
                     name="search"
                     placeholder= "  Search" 
                     onChange={props.onChange}>
-                </input>
-                <i className="fas fa-search"></i>
-            </form>
+                </SearchInput> 
+                
+            </SearchForm>
             
             <div className="profileIcons">
             <i className="far fa-compass"></i>
@@ -27,7 +65,7 @@ function SearchBar(props) {
             <i className="far fa-user"></i>
             </div>
             
-        </div>
+        </Header>
     );
 }
 
